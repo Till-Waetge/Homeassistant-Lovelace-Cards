0cf88bc50792951b7e5dcad4a25dc8447a1a52ff
Till Wätge <github.zarosch@zarosch.online>
2025-02-11 00:09:32 +0100

    Update simple-lock-status-card.js

--- simple-lock-status-card/simple-lock-status-card.js
+++ simple-lock-status-card/simple-lock-status-card.js
@@ -1,20 +1,34 @@
-class StatusCard extends HTMLElement {
+class BathStatusCard extends HTMLElement {
     set hass(hass) {
         if (!this.content) {
             this.innerHTML = `
                 <style>
-                    .status-card {
+                    .bath-status-card {
+                        display: flex;
+                        align-items: center;
+                        justify-content: space-between;
                         padding: 16px;
                         border-radius: 8px;
-                        text-align: center;
                         font-size: 1.5em;
                         font-weight: bold;
                         transition: background-color 0.3s ease;
                     }
+                    .bath-status-icon {
+                        font-size: 1.5em;
+                    }
+                    .bath-status-text {
+                        flex-grow: 1;
+                        text-align: right;
+                    }
                 </style>
-                <div class="status-card" id="status"></div>
+                <div class="bath-status-card" id="bath-status">
+                    <span class="bath-status-icon" id="bath-status-icon"><ha-icon icon="mdi:door-open"></ha-icon></span>
+                    <span class="bath-status-text" id="bath-text"></span>
+                </div>
             `;
-            this.content = this.querySelector('#status');
+            this.content = this.querySelector('#bath-status');
+            this.iconElement = this.querySelector('#bath-status-icon');
+            this.textElement = this.querySelector('#bath-text');
         }
 
         const entityId = this.config.entity;
@@ -23,13 +37,16 @@ class StatusCard extends HTMLElement {
         const freeText = this.config.free_text || 'Frei';
         
         if (state === 'on') {
-            this.content.textContent = occupiedText;
+            this.textElement.textContent = occupiedText;
             this.content.style.backgroundColor = 'red';
-            this.content.style.color = 'white';
+            this.content.style.color = 'white';            
+            this.iconElement.innerHTML = '<ha-icon icon="mdi:door-closed-lock"></ha-icon>';
+
         } else {
-            this.content.textContent = freeText;
+            this.textElement.textContent = freeText;
             this.content.style.backgroundColor = 'green';
             this.content.style.color = 'white';
+            this.iconElement.innerHTML = '<ha-icon icon="mdi:door-open"></ha-icon>';
         }
     }
 
@@ -45,4 +62,4 @@ class StatusCard extends HTMLElement {
     }
 }
 
-customElements.define('status-card', StatusCard);
+customElements.define('bath-status-card', BathStatusCard);
